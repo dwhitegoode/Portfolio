cd83098c97151401cb5cd7b5f59bf3f4d36e3e9c
David White-Goode <davidwhite-goode@davids-air.myfiosgateway.com>
2022-05-16 21:28:20 -0400

    commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -13,16 +13,9 @@ export default function Home() {
           <div className="profile-pic"></div>
           <h2>About me</h2>
           <p>
-            I'm a Software Engineer looking to keep learning in my field. I've
-            always loved programmatic thinking and realized in high school when
-            I took a course in Assembly coding language that software
-            development was a career that could allow me to spend my days
-            solving complex problems and challenging myself to be better. Each
-            day I work on self-improvement and my ultimate goal is to watch
-            myself change for the better. In my hobbies, I feel this change in
-            my physical abilities at the gym, and as a software engineer, I see
-            it in my ability to learn new languages, and frameworks while
-            creating more complex projects.
+            I am a Full Stack React Developer bringing a strong work ethic and aptitude to learn and apply new skills. Independently and in small teams I have created fully deployed applications using React front ends with backends written in both Django with PostgreSQL and Node, Express, and Mongoose utilizing RESTful APIs and MVC design patterns. The joy of programming is in the challenges and learning how to write efficient code to overcome them. In my previous roles in IT and being a self employed Day Trader being detail oriented and self motivated to research and improve processes is something I take pride in and extend to my approach in writing code. Outside of work I enjoy taking time to decompress at the gym, playing tennis, working on my guitar technique for progressive metal, motorcycles, and learning Japanese. I’m always looking for people to connect, network and collaborate with. Feel free to reach out.
+
+
           </p>
         </div>
       </div>
